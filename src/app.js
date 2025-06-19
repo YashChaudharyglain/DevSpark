62d50b282291eb6b667e7b0b02a63fcfc843f3be
YashChaudharyglain <yashiglasia@gmail.com>
2025-06-20 03:07:06 +0530

    exploring get ,post,delete

--- src/app.js
+++ src/app.js
@@ -1,23 +1,35 @@
 const express = require("express");
-
 const app = express();
 
-// Default route - runs for "/"
-app.use("/", (req, res) => {
-  res.send("Yash Chaudhary!");
+app.use(express.json());
+
+app.use("/", (req, res, next) => {
+  res.send("Namaste Akshay!");
+  next();
 });
 
-// Route for "/hello"
-app.use("/hello", (req, res) => {
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Akshay", lastName: "Saini" });
+});
+
+app.use("/hello", (req, res, next) => {
   res.send("Hello hello hello!");
+  next();
 });
 
-// Route for "/test"
+app.post("/user", async (req, res) => {
+  console.log(req.body);
+  res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully!");
+});
+
 app.use("/test", (req, res) => {
   res.send("Hello from the server!");
 });
 
-// Start the server
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
 });
